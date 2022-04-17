ded9028ec7c32106ff051be973eeb50b171d7f9b
Kerem KORKMAZ <korkmazkerem94@gmail.com>
2022-04-17 18:22:56 +0300

    17. Gün Ödev

---
+++ src/app/components/color/color.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Color } from 'src/app/models/colors';
+
+@Component({
+  selector: 'app-color',
+  templateUrl: './color.component.html',
+  styleUrls: ['./color.component.css']
+})
+export class ColorComponent implements OnInit {
+
+  colors:Color[]=[]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
